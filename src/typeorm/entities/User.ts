002699865abc4d5cddb9c65b29f21d7412c455b7
Bishnu Adhikari <bsnadh984@gmail.com>
2022-12-19 15:01:40 +0545

    commit

---
+++ src/typeorm/entities/User.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Profile } from './Profile';
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ nullable: true })
+  authStrategy: string;
+
+  @OneToOne(() => Profile)
+  @JoinColumn()
+  profile: Profile;
+}
---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { Put, Param } from '@nestjs/common/decorators';
+import { ParseIntPipe } from '@nestjs/common/pipes';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { CreateUserProfileDto } from 'src/users/dtos/CreateUserProfileDto';
+import { UpdateUserDto } from 'src/users/dtos/UpdateUser.dto';
+import { UsersService } from 'src/users/services/users/users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+  @Get()
+  getUsers() {
+    return this.userService.findUsers();
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Patch(':id')
+  async updateUsersById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async deleteUserById(@Param('id', ParseIntPipe) id: number) {
+    await this.userService.deleteUser(id);
+  }
+
+  @Post(':id/profiles')
+  createUserProfile(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() createUserProfileDto: CreateUserProfileDto,
+  ) {
+    return this.userService.createUserProfile(id, createUserProfileDto);
+  }
+}
